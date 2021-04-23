9762ac6509471ee62ce895b1654750255e2edd78
Enrico Pilz <kontakt@enricopilz.de>
2021-04-23 20:19:33 +0200

    py set 2 challenge 16

--- python/set2/challenge16.py
+++ python/set2/challenge16.py
@@ -16,15 +16,41 @@ SUFFIX = b'";comment2=%20like%20a%20pound%20of%20bacon"'
 def encrypt_c16(input_string):
     plain_text = PREFIX + quote_plus(input_string).encode() + SUFFIX
     aes = AES.new(KEY, AES.MODE_CBC, IV)
-    return aes.encrypt(padding(plain_text))
+    return bytearray(aes.encrypt(padding(plain_text)))
 
 
-def decrypt_c16(ciphertext):
+def decrypt_c16(ciphertext: bytearray):
     aes = AES.new(KEY, AES.MODE_CBC, IV)
-    plaintext = aes.decrypt(ciphertext)
-    print(plaintext)
+    plaintext = aes.decrypt(bytes(ciphertext))
+    return plaintext
+
+
+def is_broken_c16(ciphertext: bytearray):
+    plaintext = decrypt_c16(ciphertext)
     return b';admin=true;' in plaintext
 
 
+def chunks(content, block_size):
+    i = 0
+    while i < len(content):
+        yield content[i:i + block_size]
+        i += block_size
+
+
+def modify(ciphertext):
+    for (i, (x, y)) in enumerate(zip('g%20MCs;user', ';admin=true;')):
+        ciphertext[i] = ciphertext[i] ^ ord(x) ^ ord(y)
+    return ciphertext
+
+
 ciphertext = encrypt_c16(';admin=true;')
-print(decrypt_c16(ciphertext))
+modified = modify(ciphertext[:])
+
+print('----')
+plaintext = decrypt_c16(ciphertext)
+mod_plaintext = decrypt_c16(modified)
+
+for chunk, mod_chunk in zip(chunks(plaintext, BLOCK_SIZE), chunks(mod_plaintext, BLOCK_SIZE)):
+    print("%s    %s\n%s    %s\n" % (chunk.hex(), chunk, mod_chunk.hex(), mod_chunk))
+
+print(is_broken_c16(modified))
