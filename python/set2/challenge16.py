ad39bc4894297ebbec66086cc5352f023b616c38
Enrico Pilz <kontakt@enricopilz.de>
2021-05-05 21:14:41 +0200

    py challenge 27

--- python/set2/challenge16.py
+++ python/set2/challenge16.py
@@ -7,26 +7,32 @@ from Crypto.Cipher import AES
 from basics import padding
 
 BLOCK_SIZE = 16
-KEY = secrets.token_bytes(BLOCK_SIZE)
-IV = secrets.token_bytes(BLOCK_SIZE)
-PREFIX = b'"comment1=cooking%20MCs;userdata="'
-SUFFIX = b'";comment2=%20like%20a%20pound%20of%20bacon"'
 
 
-def encrypt_c16(input_string):
-    plain_text = PREFIX + quote_plus(input_string).encode() + SUFFIX
-    aes = AES.new(KEY, AES.MODE_CBC, IV)
-    return bytes(aes.encrypt(padding(plain_text)))
+class Oracle16:
+
+    def __init__(self):
+        self.key = secrets.token_bytes(BLOCK_SIZE)
+        self.iv = secrets.token_bytes(BLOCK_SIZE)
+        self.prefix = b'"comment1=cooking%20MCs;userdata="'
+        self.suffix = b'";comment2=%20like%20a%20pound%20of%20bacon"'
+
+    def aes_cbc(self):
+        return AES.new(self.key, AES.MODE_CBC, self.iv)
+
+    def encrypt(self, input_string):
+        plain_text = self.prefix + quote_plus(input_string).encode() + self.suffix
+        return bytes(self.aes_cbc().encrypt(padding(plain_text)))
+
+    def decrypt(self, ciphertext: bytes):
+        return self.aes_cbc().decrypt(ciphertext)
 
 
-def decrypt_c16(ciphertext: bytes):
-    aes = AES.new(KEY, AES.MODE_CBC, IV)
-    plaintext = aes.decrypt(bytes(ciphertext))
-    return plaintext
+oracle = Oracle16()
 
 
 def is_broken_c16(ciphertext: bytes):
-    plaintext = decrypt_c16(ciphertext)
+    plaintext = oracle.decrypt(ciphertext)
     return b';admin=true;' in plaintext
 
 
@@ -37,20 +43,29 @@ def chunks(content, block_size):
         i += block_size
 
 
-def modify(ciphertext: bytearray):
+def modify(ct: bytes):
+    mutable_ct = bytearray(ct)
     for (i, (x, y)) in enumerate(zip('g%20MCs;user', ';admin=true;')):
-        ciphertext[i] = ciphertext[i] ^ ord(x) ^ ord(y)
-    return ciphertext
+        mutable_ct[i] ^= ord(x) ^ ord(y)
+    return bytes(mutable_ct)
 
 
-ciphertext = encrypt_c16(';admin=true;')
-modified = modify(bytearray(ciphertext))
+def challenge16():
+    """
+    >>> challenge16()
+    """
+    ciphertext = oracle.encrypt(';admin=true;')
+    modified = modify(ciphertext)
 
-print('----')
-plaintext = decrypt_c16(ciphertext)
-mod_plaintext = decrypt_c16(modified)
+#    print('----')
+#    plaintext = decrypt_c16(ciphertext)
+#    mod_plaintext = decrypt_c16(modified)
+#    for chunk, mod_chunk in zip(chunks(plaintext, BLOCK_SIZE), chunks(mod_plaintext, BLOCK_SIZE)):
+#        print("%s    %s\n%s    %s\n" % (chunk.hex(), chunk, mod_chunk.hex(), mod_chunk))
 
-for chunk, mod_chunk in zip(chunks(plaintext, BLOCK_SIZE), chunks(mod_plaintext, BLOCK_SIZE)):
-    print("%s    %s\n%s    %s\n" % (chunk.hex(), chunk, mod_chunk.hex(), mod_chunk))
+    assert not is_broken_c16(ciphertext)
+    assert is_broken_c16(modified)
 
-assert is_broken_c16(modified)
+
+if __name__ == '__main__':
+    challenge16()
---
+++ python/set4/challenge27.py
@@ -0,0 +1,58 @@
+
+import secrets
+
+from Crypto.Cipher import AES
+
+from basics import padding, xor
+
+BLOCK_SIZE = 16
+
+
+class Oracle26:
+
+    def __init__(self):
+        self.key = secrets.token_bytes(BLOCK_SIZE)
+        self.iv = self.key
+
+    def aes_cbc(self):
+        return AES.new(self.key, AES.MODE_CBC, self.iv)
+
+    def encrypt(self, pt: bytes):
+        # ??
+#        try:
+#            plain_text = pt.decode("ascii")
+#        except UnicodeDecodeError as e:
+#            raise Exception(e, "pt")
+        return bytes(self.aes_cbc().encrypt(padding(pt)))
+
+    def decrypt(self, ct: bytes):
+        return self.aes_cbc().decrypt(ct)
+
+
+oracle = Oracle26()
+
+
+def modify(ct: bytes):
+    c1 = ct[:BLOCK_SIZE]
+    return bytes(c1 + bytes([0] * BLOCK_SIZE) + c1)
+
+
+def challenge27():
+    """
+    >>> challenge27()
+    """
+    p1 = secrets.token_bytes(BLOCK_SIZE)
+    p2 = secrets.token_bytes(BLOCK_SIZE)
+    p3 = secrets.token_bytes(BLOCK_SIZE)
+    ciphertext = oracle.encrypt(p1 + p2 + p3)
+    modified = modify(ciphertext)
+
+    rpt = oracle.decrypt(modified)
+    rpt1, rpt3 = rpt[:BLOCK_SIZE], rpt[2*BLOCK_SIZE:3*BLOCK_SIZE]
+    xor_p1_p3 = xor(rpt1, rpt3)
+
+    assert xor_p1_p3 == oracle.key
+
+
+if __name__ == '__main__':
+    challenge27()
