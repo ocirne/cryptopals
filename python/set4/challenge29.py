812f8e8763e470106d8012642aaa432281724810
Enrico Pilz <kontakt@enricopilz.de>
2021-05-09 19:29:39 +0200

    py challenge 29 good enough

--- python/set4/challenge29.py
+++ python/set4/challenge29.py
@@ -7,7 +7,7 @@ from challenge28 import SHA1
 class Oracle29:
 
     def __init__(self):
-        self.key = secrets.token_bytes(random.randint(10, 100))
+        self.key = secrets.token_bytes(random.randrange(5, 10))
         self.original_message = "Ice Ice Baby"
 
     def mac(self, message):
@@ -15,22 +15,22 @@ class Oracle29:
         return sha1.digest(self.key + message)
 
 
+# TODO Does not find a collision 100% of the time
 def challenge29():
     oracle = Oracle29()
     original_message = b'comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon'
+    additional_data = b';admin=true'
     provided_mac = oracle.mac(original_message)
 
-    orig_bit_length = len(original_message) * 8
-    glue_padding = SHA1().padding(oracle.key + original_message)
-
-    additional_data = b";admin=true"
-    original_mac = oracle.mac(original_message + glue_padding + additional_data)
-
-    a, b, c, d, e = [int(provided_mac[i:i+8], 16) for i in range(0, 40, 8)]
-    forge_sha1 = SHA1(a, b, c, d, e, 128)
-    forge_mac = forge_sha1.digest(additional_data)
-
-    assert forge_mac == original_mac
+    # guess
+    for key_length in range(5, 10):
+        glue_padding = SHA1().padding(b'#' * key_length + original_message)
+        reference_mac = oracle.mac(original_message + glue_padding + additional_data)
+        a, b, c, d, e = [int(provided_mac[i:i+8], 16) for i in range(0, 40, 8)]
+        # 128: len(key) + len(message) < 10 + 77 = 87 -> 128 = len(preprocessed message)
+        forged_mac = SHA1(a, b, c, d, e, 128).digest(additional_data)
+        if reference_mac == forged_mac:
+            print('Found a collision!')
 
 
 if __name__ == '__main__':
