b4d3a98dcfd7d5a76205d5e7485470004933597a
Enrico Pilz <kontakt@enricopilz.de>
2021-05-13 18:11:58 +0200

    Add middle man

--- python/src/cryptopals/set5/challenge34.py
+++ python/src/cryptopals/set5/challenge34.py
@@ -1,5 +1,6 @@
 import random
 import secrets
+from typing import Union
 
 from Crypto.Cipher import AES
 
@@ -18,6 +19,10 @@ def aes_cbc(priv_key: int, message: bytes) -> bytes:
 
 class Bob:
     def request_pub_key(self, p, g, A):
+        """
+        B -> M
+        Send "B"
+        """
         b = random.randrange(0, p)
         B = pow(g, b, p)
         self.s = pow(A, b, p)
@@ -25,15 +30,50 @@ class Bob:
         return B
 
     def request_message(self, ciphertext_a: bytes) -> bytes:
+        """
+        B->M
+        Send AES-CBC(SHA1(s)[0:16], iv=random(16), A's msg) + iv
+        """
         return aes_cbc(self.s, ciphertext_a)
 
 
-class Alice:
+class Mallory:
     def __init__(self, bob: Bob):
+        self.bob = bob
+
+    def request_pub_key(self, p, g, A):
+        """
+        A->M
+        Send "p", "g", "p"
+        ---
+        M->A
+        Send "p"
+        """
+        self.bob.request_pub_key(p, g, p)
+        return p
+
+    def request_message(self, ciphertext_a: bytes) -> bytes:
+        """
+        M->B
+        Relay that to B
+        ---
+        M->A
+        Relay that to A
+        """
+        ct_b = self.bob.request_message(ciphertext_a)
+        return ct_b
+
+
+class Alice:
+    def __init__(self, bob: Union[Bob, Mallory]):
         self.bob = bob
         self.pt = b"Ice Ice Baby"
 
-    def request_key(self):
+    def request_pub_key(self):
+        """
+        A -> M
+        Send "p", "g", "A"
+        """
         p = 3700001
         g = 50
         a = random.randrange(0, p)
@@ -43,6 +83,10 @@ class Alice:
         print("alice s", self.s)
 
     def request_message(self):
+        """
+        A -> M
+        Send AES-CBC(SHA1(s)[0:16], iv=random(16), msg) + iv
+        """
         ct = aes_cbc(self.s, self.pt)
         response = self.bob.request_message(ct)
         print("response", response)
@@ -50,8 +94,10 @@ class Alice:
 
 def challenge34():
     bob = Bob()
-    alice = Alice(bob)
-    alice.request_key()
+    mallory = Mallory(bob)
+    alice = Alice(mallory)
+
+    alice.request_pub_key()
     alice.request_message()
 
 
