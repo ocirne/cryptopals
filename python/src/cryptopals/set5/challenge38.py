e113d375362fecd7c5661fa817c7b6e1e4a6b515
Enrico Pilz <kontakt@enricopilz.de>
2021-05-15 16:24:49 +0200

    py challenge 38

--- python/src/cryptopals/set5/challenge38.py
+++ python/src/cryptopals/set5/challenge38.py
@@ -1,11 +1,14 @@
 import sys
 
 import secrets
+from pathlib import Path
 from typing import Union
 
-from challenge36 import sha256, hmac_sha256
+from cryptopals.set5.challenge36 import sha256, hmac_sha256
 
-# from cryptopals.set5.challenge36 import sha256, hmac_sha256
+
+CRACKLIB_SMALL = "/usr/share/dict/cracklib-small"  # ~35 seconds
+CRACKLIB_SMALLER = Path(__file__).parent / "resources/38.txt"
 
 
 def to_bytes(i: int) -> bytes:
@@ -18,6 +21,11 @@ def to_int(b: bytes) -> int:
 
 def hash_public_key(A, B):
     return to_int(sha256((str(A) + str(B)).encode()))
+
+
+def words():
+    for line in open(CRACKLIB_SMALLER):
+        yield bytes(line.strip(), "UTF-8")
 
 
 # https://datatracker.ietf.org/doc/html/rfc5054#appendix-A, 1. 1024-bit Group
@@ -33,13 +41,13 @@ N = int(
 g = 2
 k = 3
 email = b"test@example.com"
-password = b"totally secure password"
+password = b"sticky"
 
 
 class Server:
     def __init__(self):
         self.salt = secrets.token_bytes(16)
-        x = to_int(sha256(self.salt + b":" + password))
+        x = to_int(sha256(self.salt + password))
         self.v = pow(g, x, N)
 
     def _server_k(self, A, b, u):
@@ -58,15 +66,38 @@ class Server:
 
 
 class Mallory:
-    """ modifies b, B, u, salt """
+    """ modifies (b), B, u, (salt) """
 
     def __init__(self, server: Server):
         self.server = server
 
     def request_key(self, I, A):
-        return self.server.request_key(I, A)
+        self.A = A
+        self.salt, _, _ = self.server.request_key(I, A)
+        # set B = g, u = 1
+        return self.salt, g, 1
+
+    def _simulate_hmac(self, guess):
+        """
+        S = B ** (a+ u*x) = B**a * B**(u*x) mod N
+        set B = g, u = 1
+        S = g**a * g**(1*x) = A * g**x mod N
+        """
+        x = to_int(sha256(self.salt + guess))
+        S = (self.A * pow(g, x, N)) % N
+        K = sha256(to_bytes(S))
+        return hmac_sha256(K, self.salt)
+
+    def _crack_password(self, client_hmac):
+        for word in words():
+            guess_hmac = self._simulate_hmac(word)
+            if guess_hmac == client_hmac:
+                return word
 
     def validate(self, client_hmac):
+        recovered_password = self._crack_password(client_hmac)
+        print("recovered password:", recovered_password.decode())
+        # the hmac doesn't validate for the server, but can be calculated from the known password
         return self.server.validate(client_hmac)
 
 
@@ -75,7 +106,7 @@ class Client:
         self.server = server
 
     def _client_k(self, salt, a, B, u):
-        x = to_int(sha256(salt + b":" + password))
+        x = to_int(sha256(salt + password))
         S = pow(B, a + u * x, N)
         return sha256(to_bytes(S))
 
@@ -90,13 +121,13 @@ class Client:
 def challenge38():
     """
     >>> challenge38()
-    True
+    recovered password: sticky
     """
     server = Server()
     mallory = Mallory(server)
     client = Client(mallory)
 
-    return client.run()
+    client.run()
 
 
 if __name__ == "__main__":
